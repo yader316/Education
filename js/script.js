2cb02dffaa1c0f177865dd811163db9fe07fdb38
Yader Mendoza <yadtais316@gmail.com>
2019-11-13 18:12:45 -0600

    termino el menu resposive y la parte del cantacto modo resposive terminar los swipper o slider cliente y blog

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ function showSlidesClient(n) {
 }
 
 
-//js container Courses
+//js container Courses mobil
 var slideIndexCourses = 1;
 showSlidesCourses(slideIndexCourses);
 
@@ -72,4 +72,32 @@ function showSlidesCourses(n) {
   }
   slides[slideIndex-1].style.display = "flex"; 
   dots[slideIndex-1].className += " active-Courses";
+}
+
+// 
+var slideIndexClientMobil = 1;
+showSlidesClientMobil(slideIndexClientMobil);
+
+const plusSlidesClientMobil = (n) => {
+  showSlidesClientMobil(slideIndex += n);
+}
+
+const currentSlideClient = (n) => {
+  showSlidesClientMobil(slideIndex = n);
+}
+
+function showSlidesClientMobil(n) {
+  var i;
+  const slides = document.getElementsByClassName("client-container__info-client");
+  const dots = document.getElementsByClassName("client-container__dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active-client-mobil", "");
+  }
+  slides[slideIndex-1].style.display = "flex"; 
+  dots[slideIndex-1].className += " active-client-mobil";
 }
